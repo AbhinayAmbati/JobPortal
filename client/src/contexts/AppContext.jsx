fd1452911659b0f2994cb014848d2fe7387a9d27
AbhinayAmbati <abhinayambati4@gmail.com>
2025-02-11 12:30:35 +0530

    jwt update

--- client/src/contexts/AppContext.jsx
+++ client/src/contexts/AppContext.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
-import axios from 'axios';
+// import axios from 'axios';
 
 const AppContext = createContext();
 
@@ -11,27 +11,27 @@ export const AppProvider = ({ children }) => {
     return Boolean(Cookies.get("token")); 
   });
 
-  useEffect(() => {
-    const jwtToken = async () => {
-      if(user){
-        try {
-          const response = await axios.post("http://localhost:8080/api/user/jwt-token", {
-            email: Cookies.get("email"),
-          });
-          Cookies.set("token", response.data);
-          console.log("Token refreshed:", response.data);
-        } catch (error) {
-          console.error("Error refreshing token:", error);
-        }
-      };
-      };
-      // Run jwtToken every 1 second
-  const intervalId = setInterval(jwtToken, 1000);
+  // useEffect(() => {
+  //   const jwtToken = async () => {
+  //     if(user){
+  //       try {
+  //         const response = await axios.post("http://localhost:8080/api/user/jwt-token", {
+  //           email: Cookies.get("email"),
+  //         });
+  //         Cookies.set("token", response.data);
+  //         console.log("Token refreshed:", response.data);
+  //       } catch (error) {
+  //         console.error("Error refreshing token:", error);
+  //       }
+  //     };
+  //     };
+  //     // Run jwtToken every 1 second
+  // const intervalId = setInterval(jwtToken, 1000);
 
-  return () => clearInterval(intervalId);
+  // return () => clearInterval(intervalId);
   
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); 
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []); 
 
   const [username, setUsername] = useState(() => {
     return Cookies.get("username") || '';
